a9854177c24cf8c2d1f7dffdf2e75a6dc3f20699
liuzejiang <1239825268@qq.com>
2019-08-29 10:25:36 +0800

    2019/8/23 刘泽江 箱子业务记录 普通业务日志  箱子场位转移

--- guns-admin/src/main/java/com/stylefeng/guns/modular/system/controller/EchartController.java
+++ guns-admin/src/main/java/com/stylefeng/guns/modular/system/controller/EchartController.java
@@ -3,6 +3,7 @@ package com.stylefeng.guns.modular.system.controller;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.stylefeng.guns.modular.system.model.BusiWaybill;
+import com.stylefeng.guns.modular.system.model.Dict;
 import com.stylefeng.guns.modular.system.model.DictStation;
 import com.stylefeng.guns.modular.system.service.IBusiWaybillService;
 import com.stylefeng.guns.modular.system.service.IDictStationService;
@@ -10,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import springfox.documentation.schema.Entry;
 
 import java.io.BufferedReader;
 import java.io.InputStream;
@@ -53,9 +55,9 @@ public class EchartController {
             StringBuffer buffer = new StringBuffer();
             try {
                 //建立URL，把请求地址给补全，其中urlencode（）方法用于把params里的参数给取出来
-                URL url = new URL("http://api.map.baidu.com/geocoder?address=秦皇岛&output=json&key=37492c0ee6f924cb5e934fa08c6b1676");
+                //URL url = new URL("http://api.map.baidu.com/geocoder?address=秦皇岛&output=json&key=37492c0ee6f924cb5e934fa08c6b1676");
                 //打开http连接
-                /*URL url = new URL("http://api.map.baidu.com/geocoder?address="+province+"&output=json&key=37492c0ee6f924cb5e934fa08c6b1676");
+                URL url = new URL("http://api.map.baidu.com/geocoder?address="+province+"&output=json&key=37492c0ee6f924cb5e934fa08c6b1676");
                 //打开http连接*/
                 HttpURLConnection httpUrlConn = (HttpURLConnection) url.openConnection();
                 httpUrlConn.setDoInput(true);
@@ -82,6 +84,8 @@ public class EchartController {
                 float [] f_place=new float[2];
                 f_place[0]=Float.parseFloat(lng);
                 f_place[1]=Float.parseFloat(lat);
+
+                //地点经纬度
                 cmap.put(province,f_place);
                 System.out.println(JSONObject.toJSON(cmap));
 
@@ -98,9 +102,75 @@ public class EchartController {
                 e.printStackTrace();
             }
         }
+        if(!cmap.containsKey("秦皇岛"))
+        {   float [] _place=new float[2];
+            _place[0]=(float)119.608531;
+            _place[1]=(float) 39.941748;
+            cmap.put("秦皇岛",_place);
+        }
+
+        //返回秦皇岛到各地的箱数
+        List<BusiWaybill> listBW=busiWaybillService.selectList(null);
+        List<BusiWaybill> listGetBW=new ArrayList<>();
+        Map<String,Object>mapB=new HashMap<>();
+        //得到 秦皇岛——其他地方 的对象
+        for(BusiWaybill B:listBW)
+        {   if(B.getStartpoint()==null||B.getEndpiont()==null)
+            continue;
+            if(B.getStartpoint()==0&&B.getEndpiont()!=null)//0代表秦皇岛本地
+                listGetBW.add(B);
+        }
+        //给各地累加箱数
+
+        for(BusiWaybill bwb:listGetBW)
+        {
+            DictStation listStation = this.dictStationService.selectById(bwb.getEndpiont());
+
+            if(mapB.containsKey(listStation.getProvince()))
+            {
+
+                mapB.put(listStation.getProvince(),bwb.getBoxnumber()+Integer.parseInt(mapB.get(listStation.getProvince()).toString()));
+            }
+            else
+            {
+                mapB.put(listStation.getProvince(),bwb.getBoxnumber());
+            }
+        }
+        //返回markpoint使用
+        List<Map<String,Object>> list2=new ArrayList<>();
+        for(Map.Entry<String,Object> me:mapB.entrySet()) {
+            Map<String,Object> mf=new HashMap<>();
+            mf.put("name", me.getKey());
+            mf.put("value", me.getValue());
+            list2.add(mf);
+        }
+        //
+
+        List<List<Map<String,Object>>> list4=new ArrayList<>();
+        for(Map<String,Object> mp2:list2)
+        {   List<Map<String,Object>> list3=new ArrayList<>();
+            Map<String,Object> mf=new HashMap<>();
+            mf.put("name","秦皇岛");
+            list3.add(mf);
+            Map<String,Object> mf2=new HashMap<>();
+            mf2.put("name", mp2.get("name"));
+            mf2.put("value",mp2.get("value"));
+            list3.add(mf2);
+            list4.add(list3);
+        }
 
 
-        return  busiWaybillService.selectList(null);
+
+        List<Object> reList=new ArrayList<>();
+        reList.add(cmap);//返回各地经纬度
+        reList.add(list2);//返回箱数
+                                                        /*
+                                                        {
+                                                            "name":"江苏省",
+                                                            "value":205
+                                                        }*/
+        reList.add(list4);//返回这样的格式 [{name:'天津'}, {name:'秦皇岛',value:95}],
+                                    //[{name:'河北省'}, {name:'北京',value:90}]
+        return  reList;//一个接口返回三组数据
     }
-
 }
